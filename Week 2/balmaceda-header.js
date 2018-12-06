940d2b493e86977b64c00ee0ca720b8bdefa22f0
Griselda Balmaceda <gzbalmaceda@my365.bellevue.edu>
2018-12-05 22:19:14 -0600

    added gitingor editor config and header

---
+++ Week 2/balmaceda-header.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   5 December 2018
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../header.js');
+
+console.log(header.display("Griselda", "Balmaceda", "Week 2"));
