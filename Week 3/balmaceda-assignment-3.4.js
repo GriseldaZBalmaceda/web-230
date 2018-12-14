9291298ea1edf2a17dd6f81f705fefec9eb7ecd5
a162210 <griselda.balmaceda@pmic.com>
2018-12-14 14:55:26 -0600

    finished assignment 3.4

--- Week 3/balmaceda-assignment-3.4.js
+++ Week 3/balmaceda-assignment-3.4.js
@@ -10,35 +10,37 @@
 
 const header = require('../balmaceda-header.js');
 
-console.log(header.display("Griselda", "Balmaceda", "Assignment 3.3"));
+console.log(header.display("Griselda", "Balmaceda", "Assignment 3.4"));
 
 console.log('\n')
 
-let num=6;
+let num = 6;
 
-for(var i =0;i<9;i++){
+for (var i = 0; i < 10; i++) {
 
-    function randomNumber() {
-        return Math.floor((Math.random() * 10) + 1)
-      }
+  let num2 = Math.floor((Math.random() * 10) + 1)
+   match(num, num2)
+  if (match(num, num2)) {
+    logMatch(num, num2);
+  } else {
+    logMisMatch(num, num2);
+  }
 }
 
-function match(param1,param2){
-    if (param1===param2){
-    
+function match(param1, param2) {
+  if (param1 === param2) {
+
     return true
-    }
-    else {
-    
-      return false
-    }
-    }
-    
-    
-    function logMisMatch(param1,param2){
-      console.log(param1 + " does not match " + param2 + '.');
-    }
-    function logMatch(param1,param2){
-      console.log(param1 + " does match " + param2 + '.');
-    }
-    
+  } else {
+
+    return false
+  }
+}
+
+function logMisMatch(param1, param2) {
+  console.log(param1 + " does not match " + param2 + '!');
+}
+
+function logMatch(param1, param2) {
+  console.log(param1 + " does match " + param2 + '!');
+}
