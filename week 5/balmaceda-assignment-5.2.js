aefca16f6f1841dd2abf3770f9cc6799e5688e56
a162210 <griselda.balmaceda@pmic.com>
2019-01-09 14:48:09 -0600

    commiting assignment 5.2

---
+++ week 5/balmaceda-assignment-5.2.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  Assignment 5.2
+; Author: Griselda Balmaceda
+; Date:   9 January 2019
+; Description: Practicing Predicates
+;===========================================
+*/
+
+
+const header = require('../balmaceda-header.js');
+
+console.log(header.display("Griselda", "Balmaceda", "Assignment 5.2"));
+
+console.log('\n')
+
+
+var foodArray=['pizza','tacos','pad-see-ew','sushi','ramen']
+
+foodArray.forEach(function(x){
+    console.log(x)
+})
