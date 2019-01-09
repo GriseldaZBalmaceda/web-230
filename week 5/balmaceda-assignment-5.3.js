e353af44ddd1b0fc91e74d6b969ad585de2ab3f5
a162210 <griselda.balmaceda@pmic.com>
2019-01-09 15:08:07 -0600

    committing assignment 5.3

---
+++ week 5/balmaceda-assignment-5.3.js
@@ -0,0 +1,49 @@
+/*
+============================================
+; Title:  Assignment 5.2
+; Author: Griselda Balmaceda
+; Date:   9 January 2019
+; Description: Object Collections
+;===========================================
+*/
+
+const header = require('../balmaceda-header.js');
+
+console.log(header.display("Griselda", "Balmaceda", "Assignment 5.2"));
+
+console.log('\n')
+
+var composers= [
+    {
+        lastName:'Beethoven',
+        genre:'Classical',
+        rating:8,
+
+    },
+
+    {
+        lastName:'Mozart',
+        genre:'Classical',
+        rating:10,
+    },
+
+    {
+        lastName:"Bach",
+        genre:"Classical",
+        rating:9,
+    },
+    {
+        lastName:"Hayden",
+        genre:"Classical",
+        rating:6,  
+    },
+    {
+        lastName:"Schubert",
+        genre:"Classical",
+        rating:5,  
+    }
+]
+
+composers.forEach(function(composer){
+    console.log(composer.lastName +", "+ composer.genre+", "+composer.rating +"\n" )
+})
